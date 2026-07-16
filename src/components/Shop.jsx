a4289921b7ab1871c4d0c5149919032a115237c7
beckyshaz <sharonsakah8@gmail.com>
2026-07-16 16:09:55 +0300

    created a component that displays products that are in the online store

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,73 +1,18 @@
-import { useState, useEffect } from "react"
+import { useState } from "react"
 
-export default function Shop() {
-    const [products, setProducts] = useState([]);
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(null);
+export default function Shop({products, onAddToCart}) {
 
-    useEffect(() => {
+    const [quantity, setQuantity] = useState(0);
 
-       /* async function fetchData() {
-            const responses = await Promise.all([fetch('https://dummyjson.com/products/category/womens-dresses'),
-                fetch('https://dummyjson.com/products/category/womens-bags'),
-                fetch('https://dummyjson.com/products/category/womens-shoes'),
-        ])
+    function increaseInputHandler() {
+        setQuantity((quantity) => quantity + 1)
+        
+    }
 
-        console.log(responses);
-
-        const data = await Promise.all(
-            responses.map((res) => res.json());
-
-        );
-
-        const products = data.flatMap((d) => d.products)
-        */
-      
-        setTimeout(() => {
-            setLoading(true);
-            const dresses = fetch('https://dummyjson.com/products/category/womens-dresses');
-            const bags = fetch('https://dummyjson.com/products/category/womens-bags');
-            const shoes = fetch('https://dummyjson.com/products/category/womens-shoes');
-            const jewellery = fetch('https://dummyjson.com/products/category/womens-jewellery');
-            const watches = fetch('https://dummyjson.com/products/category/womens-watches')
-
-            Promise.all([dresses, bags, shoes, jewellery, watches]).then((responses) => {
-            return Promise.all(responses.map((res) => {
-                    if (!res.ok) {
-                        throw new Error('An error occured');
-                    }
-                    return res.json();
-
-                }))
-                
-                
-            }) 
-
-            .then((data) => {
-            const products = data.flatMap((d) => d.products)
-                console.log(products);
-                setProducts(products);
-                setError(null);
-
-                
-            })
-            
-            .catch((error) => {
-                setError(error);
-                setProducts([]);
-                
-            })
-            .finally(() => setLoading(false));
-                
-
-            }, 1000)
-            
-    }, [])
-
-    if (loading) return <p>loading ...</p>
-
-    if (error) return <p>Network error occured</p>
-
+    function decreaseInputHandler() {
+        setQuantity((quantity) => quantity - 1)
+    }
+  
     return (
         <div>
            { products.map((product) => {
@@ -76,17 +21,18 @@ export default function Shop() {
                     <img src={product.images[0]} alt={product.description} width={200} />
                     <div className="quantity">
                         <label htmlFor={product.title}>Enter quantity</label>
-                        <input type="number" id={product.title} name={product.title} />
+                        <input type="number" id={product.title} name={product.title} value={quantity} 
+                        onChange={(event) => setQuantity(event.currentTarget.value)} />
                         <div>
                             <div>
                                 
-                                <input type="button" id="increament" value="+"/>
+                                <input type="button" id="increament" value="+" onClick={increaseInputHandler}/>
  
-                                <input type="button" id="decreament" value="-"/>
+                                <input type="button" id="decreament" value="-" onClick={decreaseInputHandler}/>
                             </div>
                             <div>
-                                <p>{product.price}$</p>
-                                <button>Add to cart</button>
+                                <p>Price: {product.price}$</p>
+                                <button onClick={() => onAddToCart(product.id)}>Add to cart</button>
                             </div>
                             
                         </div>
