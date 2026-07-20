080e8fab66dc90a60363c88fabf528fafa4dd678
beckyshaz <sharonsakah8@gmail.com>
2026-07-20 13:24:11 +0300

    adding css classes to jsx to style them using css modules

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,29 @@
-import { Link } from "react-router"
 
-export function Navbar() {
+import { Link } from "react-router"
+import styles from "./Navbar.module.css";
+
+function Navbar() {
     return (
-        <nav>
-        <ul>
-        <li><Link to="home">Home</Link></li>
-        <li><Link to="shop">Shop</Link></li>
-        <li><Link to="cart">Cart</Link></li>
-        </ul>
-        
-        </nav>
+        <header className={styles.header}>
+            <nav>
+                <ul className={styles.ul}>
+                <li className={styles.list}><Link to="home" className={styles.link}>Home</Link></li>
+                <li className={styles.list}><Link to="shop" className={styles.link}>Shop</Link></li>
+                <li className={styles.list}><Link to="cart" className={styles.link}>Cart</Link></li>
+                </ul>
+            </nav>
+
+            <div className={styles.welcome}>
+                <div>
+                    <h1>Welcome to our shop</h1>
+                    <p>Discover the latest street ware fashion...</p>
+                    <button className={styles.btn}>Shop now!</button>
+                </div>
+                
+            </div>
+            
+        </header>
     )
 }
+
+export default Navbar;
